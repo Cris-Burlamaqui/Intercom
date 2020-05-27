9e9ce84d2f87bba04bccc644f0524a2506d26832
Cris Burlamaqui <crica.b@gmail.com>
2020-05-27 16:53:01 +0100

    Data ingestion and first version of showing the list of customers.

--- Invitation/Invitation/FindCustomerViewController.swift
+++ Invitation/Invitation/FindCustomerViewController.swift
@@ -11,26 +11,54 @@ import UIKit
 class FindCustomerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
-    @IBOutlet weak var loading: UIActivityIndicatorView!
     @IBOutlet weak var customerTableView: UITableView!
+    
+    var customerList:[Customer] = []
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func findCustomerAction(_ sender: UIButton) {
+        ingestCustomerJSON()
     }
     
+    //MARK: Ingested customer json
+    
+    private func ingestCustomerJSON() {
+        
+        do {
+            customerList = try Array<Customer>.init(fileName: "customers")
+            customerList = customerList.sorted { $0.user_id < $1.user_id }
+        } catch  {
+            print(error)
+        }
+        
+    }
+    
+    // MARK: Find near customer
+    
+    @IBAction func findCustomerAction(_ sender: UIButton) {
+        
+        customerTableView.isHidden = false
+        customerTableView.alpha = 0.0
+        
+        UIView.animate(withDuration: 1) {
+            self.customerTableView.alpha = 1
+            self.customerTableView.reloadData()
+        }
+    }
+    
+    // MARK: TableView methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return customerList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "customerCell") as! CustomerCell
+        
+        cell.customerID.text = String(customerList[indexPath.row].user_id)
+        cell.customerName.text = customerList[indexPath.row].name
         
         return cell
     }
